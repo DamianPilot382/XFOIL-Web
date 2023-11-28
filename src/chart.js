b7c382b9c07d90fa6450ede44fc1bf34d78fba15
Damian Ugalde <damianugalde@gmail.com>
2023-11-28 01:22:34 -0800

    Working Airfoil Plotter

--- src/chart.js
+++ src/chart.js
@@ -3,21 +3,45 @@ import * as fs from 'fs';
 
 
 
-let csv = fs.readFileSync("f.csv", "utf8");
+var airfoil = "NACA 0012";
+
+let csv = fs.readFileSync('NACA0012.csv', "utf8");
+
+
 let data = d3.csvParse(csv);
 
-var playerLabels = data.map(function (d) { return d.Name });
-var weeksData = data.map(function (d) { return d.Weeks });
+var xCoord = data.map(function (d) { return d.x });
+var yCoord = data.map(function (d) { return d.y });
 
 
 var chart = new Chart('chart', {
-    type: 'bar',
+    type: 'scatter',
     data: {
-        labels: playerLabels,
-        datasets: [
-            {
-                data: weeksData
+        labels:xCoord,
+        datasets: [{
+            label: airfoil,
+            data: yCoord,
+            borderColor: 'black',
+            borderWidth: 1,
+            pointBackgroundColor: ['#000'],
+            pointBorderColor: ['#000'],
+            pointRadius: 3,
+            pointHoverRadius: 5,
+            showLine: true,
+            fill:true,
+        }]
+    },
+    options: {
+        responsive:true,
+        aspectRatio:1,
+        legend: false,
+        tooltips: false,
+        scales: {
+
+            y:{
+                min: -1,
+                max: 1
             }
-        ]
+        }
     }
 });
