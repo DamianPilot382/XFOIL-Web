54e10c17bfdd9f079315a20f37fa427999625f17
Damian Ugalde <damianugalde@gmail.com>
2023-11-28 00:53:30 -0800

    Working Local CSV

--- src/chart.js
+++ src/chart.js
@@ -1,24 +1,23 @@
 import Chart from 'chart.js/auto'
-
-d3.csv('https://s3-us-west-2.amazonaws.com/s.cdpn.io/2814973/atp_wta.csv').then(makeChart);
-
-function makeChart(players) {
-    // players is an array of objects where each object represents a player
-    console.log("HERE");
-
-    var playerLabels = players.map(function (d) { return d.Name });
-    var weeksData = players.map(function (d) { return d.Weeks });
+import * as fs from 'fs';
 
 
-    var chart = new Chart('chart', {
-        type: 'bar',
-        data: {
-            labels: playerLabels,
-            datasets: [
-                {
-                    data: weeksData
-                }
-            ]
-        }
-    });
-}
+
+let csv = fs.readFileSync("f.csv", "utf8");
+let data = d3.csvParse(csv);
+
+var playerLabels = data.map(function (d) { return d.Name });
+var weeksData = data.map(function (d) { return d.Weeks });
+
+
+var chart = new Chart('chart', {
+    type: 'bar',
+    data: {
+        labels: playerLabels,
+        datasets: [
+            {
+                data: weeksData
+            }
+        ]
+    }
+});
