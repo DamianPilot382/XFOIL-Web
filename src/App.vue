3eb0f0519e71bea074b6d4616c388dbe76c19aff
Damian Ugalde <damianugalde@gmail.com>
2024-02-13 23:54:54 -0800

    Clean Vuetify install

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,15 @@
       <v-img alt="XFoil" contain src="./assets/logo.png" max-height="3rem"></v-img>
     </v-app-bar>
     <v-main>
-      <!-- two column layout with graph on left and input on right -->
+      <h1>Hello there</h1>
       <v-container fluid>
         <v-row>
+          <v-col cols="12">
+            <b-button variant="success">Hello World!</b-button>
+            <!-- <Dropper /> -->
+          </v-col>
+        </v-row>
+        <!-- <v-row>
           <v-col cols="7">
             <AirfoilPanel />
           </v-col>
@@ -21,17 +27,19 @@
           <v-col cols="5">
             <InputPanel />
           </v-col>
-        </v-row>
+        </v-row> -->
       </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script setup>
-  import InputPanel from './components/InputPanel.vue';
-  import GraphPanel from './components/GraphPanel.vue';
-  import NACAInputPanel from './components/NACAInputPanel.vue';
-  import AirfoilPanel from './components/AirfoilPanel.vue';
+  // import InputPanel from './components/InputPanel.vue';
+  // import GraphPanel from './components/GraphPanel.vue';
+  // import NACAInputPanel from './components/NACAInputPanel.vue';
+  // import AirfoilPanel from './components/AirfoilPanel.vue';
+  // import Dropdown from './components/Dropdown.vue';
+  // import Dropper from './components/Dropper.vue';
 </script>
 
 <style >
