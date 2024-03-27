4bd623436a44bf3202478b374d37c53a6a33d83f
Damian Ugalde <damianugalde@gmail.com>
2024-03-26 21:07:32 -0700

    Add Airfoil Test Data File

--- src/components/Graphs/AirfoilGraph.vue
+++ src/components/Graphs/AirfoilGraph.vue
@@ -3,34 +3,11 @@ import { Chart, PointElement, LineElement, LinearScale } from 'chart.js';
 import dragData from 'chartjs-plugin-dragdata';
 import { Scatter } from 'vue-chartjs';
 import { useAirfoilDataStore } from "../../stores/airfoilData.js";
-import { convertChartToJson, convertJsonToChart } from "../../utils/AirfoilDataConvert.js";
-import { ref } from 'vue';
+import { airfoilTestData } from "../../utils/airfoilTestData.js";
 
 var airfoilData = useAirfoilDataStore().airfoilData;
 
-airfoilData.value = { 
-  datasets: [{
-    data: [ {x: 1.000000, y: -0.000000},
-            {x: 0.975680, y:  0.004571},
-            {x: 0.905027, y:  0.017080},
-            {x: 0.794797, y:  0.034501},
-            {x: 0.655622, y:  0.053079},
-            {x: 0.501044, y:  0.068901},
-            {x: 0.346213, y:  0.078064},
-            {x: 0.206410, y:  0.077433},
-            {x: 0.094661, y:  0.066001},
-            {x: 0.016983, y:  0.033378},
-            {x: 0.000000, y:  0.000000},
-            {x: 0.031960, y: -0.016196},
-            {x: 0.096322, y: -0.026082},
-            {x: 0.205805, y: -0.037989},
-            {x: 0.344770, y: -0.041040},
-            {x: 0.498956, y: -0.036802},
-            {x: 0.653395, y: -0.028263},
-            {x: 0.792988, y: -0.018278},
-            {x: 0.903990, y: -0.009042},
-            {x: 0.975377, y: -0.002425},
-            {x: 1.000000, y:  0.000000} ]}]};
+airfoilData.value = airfoilTestData;
 
 var config = {
   aspectRatio: 6,
@@ -64,17 +41,6 @@ var config = {
 
 Chart.register(LinearScale, PointElement, LineElement, dragData);
 
-var converted = ref({});
-function convertToJson(){
-  converted.value = convertChartToJson(airfoilData.value)
-  console.log(converted.value);
-}
-
-function convertToChart(){
-  airfoilData.value = convertJsonToChart(converted.value);
-  console.log(convertJsonToChart(converted.value));
-}
-
 </script>
   
 <template>
@@ -85,9 +51,6 @@ function convertToChart(){
         <h2>Airfoil Plot</h2>
 
         <Scatter :data="airfoilData.value" :options="config"/>
-        
-        <v-btn @click="convertToJson">JSON</v-btn>
-        <v-btn @click="convertToChart">Chart</v-btn>
 
       </v-card>
     </v-responsive>
